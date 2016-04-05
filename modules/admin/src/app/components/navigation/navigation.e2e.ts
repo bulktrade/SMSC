0c5d9d266d0e2bf22911cfd15eab37fededf896d
HVND <yura.kuba@gmail.com>
2016-04-05 15:49:30 +0300

    changed protractor and karma test

--- modules/admin/src/app/components/navigation/navigation.e2e.ts
+++ modules/admin/src/app/components/navigation/navigation.e2e.ts
@@ -5,6 +5,11 @@ describe('Navigation', () => {
     this.navigator.get();
   });
 
+  it('log in for user admin', () => {
+    this.navigator.login();
+    expect(true).toBeTruthy();
+  });
+
   it('should have a title', () => {
     let result  = 'Angular2 Admin';
     expect(this.navigator.getTitle()).toBe(result);
@@ -23,8 +28,11 @@ describe('Navigation', () => {
 });
 
 class NavigationTest {
-  elemSidebar = element(by.className('side-bar'));
+  elemSidebar   = element(by.className('side-bar'));
   elemDashboard = element(by.className('dashboard'));
+  elemUsername  = element(by.className('username'));
+  elemPassword  = element(by.className('password'));
+  elemSubmitBtn = element(by.className('btn'));
 
   get() {
     browser.get('/');
@@ -42,4 +50,9 @@ class NavigationTest {
     return browser.getTitle();
   }
 
+  login() {
+    this.elemUsername.sendKeys('admin');
+    this.elemPassword.sendKeys('admin');
+    this.elemSubmitBtn.submit();
+  }
 }
