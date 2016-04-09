33af0bb04b323a3e747ba0439e663c98b42b5915
Sergej Kunz <info@bulktrade.de>
2016-04-09 11:52:52 +0200

    only chrome enabled for integration tests

--- modules/admin/protractor.conf.js
+++ modules/admin/protractor.conf.js
@@ -38,49 +38,49 @@ exports.config = {
 			shardTestFiles: true,
 			build: process.env.TRAVIS_BUILD_NUMBER,
 			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		},
-		{
-			browserName: 'firefox',
-			platform: 'OS X 10.11',
-			name: "firefox-tests",
-			shardTestFiles: true,
-			build: process.env.TRAVIS_BUILD_NUMBER,
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		},
-		{
-			browserName: 'safari',
-			platform: 'OS X 10.11',
-			name: "safari-tests",
-			shardTestFiles: true,
-			build: process.env.TRAVIS_BUILD_NUMBER,
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		},
-		{
-			browserName: 'MicrosoftEdge',
-			name: "edge-tests",
-			shardTestFiles: true,
-			build: process.env.TRAVIS_BUILD_NUMBER,
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		},
-		{
-			platformName: 'iOS',
-			browserName: '',
-			name: "ios-tests",
-			app: 'safari',
-			deviceName: 'iPhone Simulator',
-			shardTestFiles: true,
-			build: process.env.TRAVIS_BUILD_NUMBER,
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-
-		},
-		{
-			platformName: 'Android',
-			browserName: 'Browser',
-			name: "android-tests",
-			deviceName: 'Android Emulator',
-			build: process.env.TRAVIS_BUILD_NUMBER,
-			'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
-		}
+		} //,
+		// {
+		// 	browserName: 'firefox',
+		// 	platform: 'OS X 10.11',
+		// 	name: "firefox-tests",
+		// 	shardTestFiles: true,
+		// 	build: process.env.TRAVIS_BUILD_NUMBER,
+		// 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		// },
+		// {
+		// 	browserName: 'safari',
+		// 	platform: 'OS X 10.11',
+		// 	name: "safari-tests",
+		// 	shardTestFiles: true,
+		// 	build: process.env.TRAVIS_BUILD_NUMBER,
+		// 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		// },
+		// {
+		// 	browserName: 'MicrosoftEdge',
+		// 	name: "edge-tests",
+		// 	shardTestFiles: true,
+		// 	build: process.env.TRAVIS_BUILD_NUMBER,
+		// 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		// },
+		// {
+		// 	platformName: 'iOS',
+		// 	browserName: '',
+		// 	name: "ios-tests",
+		// 	app: 'safari',
+		// 	deviceName: 'iPhone Simulator',
+		// 	shardTestFiles: true,
+		// 	build: process.env.TRAVIS_BUILD_NUMBER,
+		// 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		//
+		// },
+		// {
+		// 	platformName: 'Android',
+		// 	browserName: 'Browser',
+		// 	name: "android-tests",
+		// 	deviceName: 'Android Emulator',
+		// 	build: process.env.TRAVIS_BUILD_NUMBER,
+		// 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+		// }
 	],
 
 	/**
