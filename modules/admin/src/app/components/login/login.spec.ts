2a7962a86d152c0b3b0ccc12df26a90189401520
Sergej Kunz <info@bulktrade.de>
2016-04-10 01:38:51 +0200

    fixes

--- modules/admin/src/app/components/login/login.spec.ts
+++ modules/admin/src/app/components/login/login.spec.ts
@@ -14,13 +14,4 @@ describe('Login', () => {
     login.authentication('admin', 'admin');
     expect(login.authentication).toHaveBeenCalledWith('admin', 'admin');
   });
-
-  it('sha256', () => {
-    spyOn(login, 'sha256');
-    login.sha256('admin');
-    expect(login.sha256).toHaveBeenCalled();
-
-    login.sha256('admin');
-    expect(login.sha256).toHaveBeenCalledWith('admin');
-  });
 });
