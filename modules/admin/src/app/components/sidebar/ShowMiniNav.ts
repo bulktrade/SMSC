a2018ff2300809f4c4ac215000f3761a0f733311
HVND <dimakuba124@gmail.com>
2016-05-12 12:23:22 +0300

    cleanup

--- modules/admin/src/app/components/sidebar/ShowMiniNav.ts
+++ modules/admin/src/app/components/sidebar/ShowMiniNav.ts
@@ -1,15 +1,10 @@
 import {LocalStorage} from "../../../assets/js/angular2-localstorage/WebStorage";
 
-declare var Reflect;
-
 // save state short navigation after reload page
 export class ShowMiniNav {
     @LocalStorage()
     public showNav:boolean;
 
     constructor() {
-    }
-
-    ngOnInit() {
     }
 }
