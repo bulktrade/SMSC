27d5b29145025e3eca3c48eabca107d81e180baf
HVND <dimakuba124@gmail.com>
2016-05-25 20:04:24 +0300

    cleanup

--- modules/admin/src/app/common/mock.service.ts
+++ modules/admin/src/app/common/mock.service.ts
@@ -2,9 +2,7 @@ export class Mock {
     static mock(constr?, name?) {
         let keys = [];
         for (let key in constr) {
-            // if (constr.hasOwnProperty(key)) {
-                keys.push(key);
-            // }
+            keys.push(key);
         }
 
         return keys.length > 0 ? jasmine.createSpyObj( name || 'mock', keys ) : {};
