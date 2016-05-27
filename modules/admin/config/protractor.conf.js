f65ed355b6a6504e1f7cdcf1a22ae5d674eadb87
Sergej Kunz <info@bulktrade.de>
2016-05-27 16:13:02 +0200

    protractor fixed

--- modules/admin/config/protractor.conf.js
+++ modules/admin/config/protractor.conf.js
@@ -2,6 +2,9 @@ require('ts-node/register');
 var helpers = require('./helpers');
 
 exports.config = {
+    sauceUser: process.env.SAUCE_USERNAME,
+    sauceKey: process.env.SAUCE_ACCESS_KEY,
+
     baseUrl: 'http://localhost:3000/',
 
     // use `npm run e2e`
@@ -22,14 +25,75 @@ exports.config = {
         includeStackTrace: false,
         defaultTimeoutInterval: 400000
     },
-    directConnect: true,
+    // directConnect: true,
 
-    capabilities: {
-        'browserName': 'chrome',
-        'chromeOptions': {
-            'args': ['show-fps-counter=true']
-        }
-    },
+    // capabilities: {
+    //     'browserName': 'chrome',
+    //     'chromeOptions': {
+    //         'args': ['show-fps-counter=true']
+    //     }
+    // },
+
+    multiCapabilities: [
+        {
+         browserName: 'chrome',
+         platform: 'OS X 10.11',
+         name: "chrome-tests",
+         shardTestFiles: true,
+         build: process.env.TRAVIS_BUILD_NUMBER,
+         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+         },
+         {
+         browserName: 'firefox',
+         platform: 'OS X 10.11',
+         name: "firefox-tests",
+         shardTestFiles: true,
+         build: process.env.TRAVIS_BUILD_NUMBER,
+         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+         },
+         {
+         browserName: 'safari',
+         platform: 'OS X 10.11',
+         name: "safari-tests",
+         shardTestFiles: true,
+         build: process.env.TRAVIS_BUILD_NUMBER,
+         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+         },
+         {
+         browserName: 'MicrosoftEdge',
+         name: "edge-tests",
+         shardTestFiles: true,
+         build: process.env.TRAVIS_BUILD_NUMBER,
+         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+         },
+         {
+         browserName: 'opera',
+         platform: 'OS X 10.11',
+         name: "opera-tests",
+         shardTestFiles: true,
+         build: process.env.TRAVIS_BUILD_NUMBER,
+         'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+         } //,*/
+        // {
+        // 	platformName: 'iOS',
+        // 	browserName: '',
+        // 	name: "ios-tests",
+        // 	app: 'safari',
+        // 	deviceName: 'iPhone Simulator',
+        // 	shardTestFiles: true,
+        // 	build: process.env.TRAVIS_BUILD_NUMBER,
+        // 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+        //
+        // },
+        // {
+        // 	platformName: 'Android',
+        // 	browserName: 'Browser',
+        // 	name: "android-tests",
+        // 	deviceName: 'Android Emulator',
+        // 	build: process.env.TRAVIS_BUILD_NUMBER,
+        // 	'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER
+        // }
+    ],
 
     onPrepare: function() {
         browser.ignoreSynchronization = true;
