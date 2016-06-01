89d88f1cedd40fbf2dc829720749ff3ad8fcac6a
Sergej Kunz <s.kunz@bulktrade.org>
2016-06-01 07:59:20 +0200

    fixed tests

--- modules/core/src/test/java/io/smsc/orientdb/ServerTest.java
+++ modules/core/src/test/java/io/smsc/orientdb/ServerTest.java
@@ -7,9 +7,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-/**
- * Created by deacix on 08.02.16.
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {Application.class})
 @WebAppConfiguration
