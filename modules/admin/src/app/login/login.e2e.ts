312f8949f6150dbf62555336ef30b7690e73240c
Sergej Kunz <info@deacix.de>
2016-06-02 09:22:02 +0200

    Update login.e2e.ts

--- modules/admin/src/app/login/login.e2e.ts
+++ modules/admin/src/app/login/login.e2e.ts
@@ -4,7 +4,7 @@ describe('Login page', () => {
     let ptor = protractor.wrapDriver(browser.driver);
 
     beforeEach(() => {
-        browser.ignoreSynchronization = true;
+        browser.ignoreSynchronization = false;
         this.lognpg = new LoginTest();
         ptor = protractor.wrapDriver(browser.driver);
     });
