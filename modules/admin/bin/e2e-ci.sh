86eaedb60b112242f74c52b6f055ae4a9bdf22d8
Sergej Kunz <info@deacix.de>
2016-06-02 08:58:26 +0200

    Update e2e-ci.sh

--- modules/admin/bin/e2e-ci.sh
+++ modules/admin/bin/e2e-ci.sh
@@ -2,9 +2,10 @@
 set +e
 npm run orientdb:dev:clean
 npm run orientdb:dev
-npm run start > /dev/null 2>&1&
-sleep 60
 set -e
+
+npm run start&
+sleep 60
 
 export DBUS_SESSION_BUS_ADDRESS=/dev/null
 
