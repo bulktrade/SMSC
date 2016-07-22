84dceba3bfd871e882ff541e308e9eb54bb38f9c
Dmitrij Kuba <dimakuba124@gmail.com>
2016-07-22 15:41:59 +0300

    update sql for customer class

--- modules/core/src/main/resources/db/migration/1464469001.sql
+++ modules/core/src/main/resources/db/migration/1464469001.sql
@@ -18,8 +18,31 @@ CREATE PROPERTY CustomerContacts.mobilePhone STRING
 CREATE PROPERTY CustomerContacts.fax STRING
 CREATE PROPERTY CustomerContacts.emailAddress STRING
 
-CREATE SEQUENCE customerIdSeq TYPE ORDERED
-
 CREATE PROPERTY Customer.contacts LINKSET CustomerContacts
 CREATE PROPERTY Customer.users LINKSET OUser
 CREATE PROPERTY Customer.parentCustomer LINK Customer
+
+CREATE SEQUENCE customerIdSeq TYPE ORDERED
+
+CREATE INDEX Customer.customerId UNIQUE
+CREATE INDEX CustomerContacts.emailAddress UNIQUE
+
+ALTER PROPERTY Customer.customerId NOTNULL true
+ALTER PROPERTY Customer.companyName NOTNULL true
+ALTER PROPERTY Customer.street NOTNULL true
+ALTER PROPERTY Customer.street2 NOTNULL true
+ALTER PROPERTY Customer.postcode NOTNULL true
+ALTER PROPERTY Customer.country NOTNULL true
+ALTER PROPERTY Customer.city NOTNULL true
+ALTER PROPERTY Customer.contacts NOTNULL true
+ALTER PROPERTY Customer.users NOTNULL true
+ALTER PROPERTY Customer.parentCustomer NOTNULL true
+
+ALTER PROPERTY CustomerContacts.type NOTNULL true
+ALTER PROPERTY CustomerContacts.salutation NOTNULL true
+ALTER PROPERTY CustomerContacts.firstname NOTNULL true
+ALTER PROPERTY CustomerContacts.surename NOTNULL true
+ALTER PROPERTY CustomerContacts.phone NOTNULL true
+ALTER PROPERTY CustomerContacts.mobilePhone NOTNULL true
+ALTER PROPERTY CustomerContacts.emailAddress NOTNULL true
+
