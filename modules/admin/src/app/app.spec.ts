252d7af6df8c8b018b03d144d1cd0c8568f0deb5
Dmitrij Kuba <dimakuba124@gmail.com>
2016-07-27 10:51:32 +0300

    fix

--- modules/admin/src/app/app.spec.ts
+++ modules/admin/src/app/app.spec.ts
@@ -4,15 +4,13 @@ import {
   it
 } from '@angular/core/testing';
 
+// Load the implementations that should be tested
 import { App } from './app.component';
 import { AppState } from './app.service';
 
 describe('App', () => {
+  // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
   ]);
-
-  it('should have a url', inject([ App ], (app) => {
-    expect(app.url).toEqual('http://www.smsc.io/');
-  }));
 
 });
