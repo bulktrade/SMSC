a6fa0d7c14a4e0a51df37f5994b83e412348e194
Sergej Kunz <info@bulktrade.de>
2016-08-01 20:28:51 +0200

    spring cloud updated

--- modules/admin/src/app/crud/crudCreate/crud.create.ts
+++ modules/admin/src/app/crud/crudCreate/crud.create.ts
@@ -2,9 +2,9 @@ import { Component } from "@angular/core";
 import { TranslatePipe, TranslateService } from "ng2-translate/ng2-translate";
 import { Router } from "@angular/router";
 import { CrudService } from "../crud.service";
-import {MultipleSelect} from "../directives/multipleSelect";
-import {CrudLinkset} from "../crudLinkset/crud.linkset";
-import {MdCheckbox} from "@angular2-material/checkbox/checkbox";
+import { MultipleSelect } from "../directives/multipleSelect";
+import { CrudLinkset } from "../crudLinkset/crud.linkset";
+import { MdCheckbox } from "@angular2-material/checkbox/checkbox";
 
 @Component({
     selector: 'crud-create',
@@ -18,7 +18,7 @@ import {MdCheckbox} from "@angular2-material/checkbox/checkbox";
 })
 
 export class CrudCreate {
-    public  btnName:string = 'Create';
+    public btnName:string = 'Create';
 
     constructor(public translate:TranslateService,
                 public crudService:CrudService,
