8225744a08f7258dd1259b9c62e799aa7d915f32
Dmitrij Kuba <dimakuba124@gmail.com>
2016-08-01 16:32:49 +0300

    directive fixed

--- modules/admin/src/app/crud/directives/multipleSelect.ts
+++ modules/admin/src/app/crud/directives/multipleSelect.ts
@@ -23,7 +23,8 @@ import {MdAnchor, MdButton} from "@angular2-material/button/button";
 
 export class MultipleSelect {
     @Input('crudService') public crudService:any;
-    @Input('title') public title:any;
+    @Input('property') public property:any;
+    @Input('rowSelectionLinkset') rowSelectionLinkset:string;
     public items;
 
     constructor(public translate:TranslateService,
@@ -32,11 +33,16 @@ export class MultipleSelect {
 
     ngOnInit() {
         this.crudService.initGridData.then((res) => {
+            this.crudService.rowSelectionLinkset = this.rowSelectionLinkset;
             this.items = [];
-            let users = this.crudService.model['users'];
+            let linkset = this.crudService.model[this.property.field];
 
-            if (users) {
-                users.forEach((item) => {
+            if (typeof linkset === 'string') {
+                linkset = linkset.split(',');
+            }
+
+            if (linkset) {
+                linkset.forEach((item) => {
                     this.items.push({
                         name: item, visible: true
                     });
@@ -46,14 +52,22 @@ export class MultipleSelect {
     }
 
     removeItem() {
-        let users = [];
+        let linkset = [];
 
         this.items.forEach((item) => {
            if (item.visible) {
-               users.push(item.name);
+               linkset.push(item.name);
            }
         });
 
-        this.crudService.model[this.title] = users;
+        this.crudService.model[this.property.field] = linkset;
+        this.crudService.isActiveLinkset = this.property.field;
+    }
+
+    addLinkset() {
+        this.crudService.showLinksetView = true;
+        this.crudService.fieldsValue = this.crudService.model;
+        this.crudService.isActiveLinkset = this.property.field;
+        this.crudService.linkedClass = this.property.linkedClass;
     }
 }
