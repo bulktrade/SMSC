f435e4df2153f445d24e475e169c6b410d5f3f0a
Sergej Kunz <info@bulktrade.de>
2016-08-05 21:33:18 +0200

    migration implemented

--- modules/core/src/main/java/io/smsc/dao/impl/MigrationDaoImpl.java
+++ modules/core/src/main/java/io/smsc/dao/impl/MigrationDaoImpl.java
@@ -2,6 +2,7 @@ package io.smsc.dao.impl;
 
 import com.orientechnologies.orient.jdbc.OrientJdbcConnection;
 import io.smsc.dao.MigrationDao;
+import io.smsc.service.OrientDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,18 @@ import java.sql.Statement;
 
 @Component
 public class MigrationDaoImpl implements MigrationDao {
-    @Autowired
-    private OrientJdbcConnection orientDBConnection;
+	@Autowired
+	private OrientDBService orientDBService;
 
-    public void upgradeDatabase(String sql) throws SQLException {
-        Statement stmt = orientDBConnection.createStatement();
-        stmt.execute(sql);
-    }
+	public void upgradeDatabase(String sql) throws SQLException {
+		OrientJdbcConnection connection = orientDBService.getJdbcConnection();
+		Statement stmt = connection.createStatement();
+
+		connection.setAutoCommit(false);
+
+		stmt.addBatch(sql);
+		stmt.executeBatch();
+
+		connection.commit();
+	}
 }
--- modules/core/src/main/resources/db/migration/1464469001.sql
+++ modules/core/src/main/resources/db/migration/1464469001.sql
@@ -1,48 +1,74 @@
-CREATE Class Customer
-CREATE PROPERTY Customer.customerId DOUBLE
-CREATE PROPERTY Customer.companyName STRING
-CREATE PROPERTY Customer.street STRING
-CREATE PROPERTY Customer.street2 STRING
-CREATE PROPERTY Customer.postcode DOUBLE
-CREATE PROPERTY Customer.country STRING
-CREATE PROPERTY Customer.city STRING
-CREATE PROPERTY Customer.vatid DOUBLE
+let customerContactClass = SELECT FROM (SELECT expand(classes) FROM metadata:schema) WHERE name = 'CustomerContact';
 
-CREATE Class CustomerContact
-CREATE PROPERTY CustomerContact.type EMBEDDEDLIST
-CREATE PROPERTY CustomerContact.salutation EMBEDDEDLIST
-CREATE PROPERTY CustomerContact.firstname STRING
-CREATE PROPERTY CustomerContact.surename STRING
-CREATE PROPERTY CustomerContact.phone STRING
-CREATE PROPERTY CustomerContact.mobilePhone STRING
-CREATE PROPERTY CustomerContact.fax STRING
-CREATE PROPERTY CustomerContact.emailAddress STRING
+if ($customerContactClass.size() == 0) {
+  console.log "CustomerContact class not exists! Start creating process."
 
-CREATE PROPERTY Customer.contacts LINKSET CustomerContact
-CREATE PROPERTY Customer.users LINKSET OUser
-CREATE PROPERTY Customer.parentCustomer LINK Customer
+  CREATE Class CustomerContact
+  CREATE PROPERTY CustomerContact.type EMBEDDEDLIST
+  CREATE PROPERTY CustomerContact.salutation EMBEDDEDLIST
+  CREATE PROPERTY CustomerContact.firstname STRING
+  CREATE PROPERTY CustomerContact.surename STRING
+  CREATE PROPERTY CustomerContact.phone STRING
+  CREATE PROPERTY CustomerContact.mobilePhone STRING
+  CREATE PROPERTY CustomerContact.fax STRING
+  CREATE PROPERTY CustomerContact.emailAddress STRING
 
-CREATE SEQUENCE customerIdSeq TYPE ORDERED
+  ALTER PROPERTY CustomerContact.type MANDATORY true
+  ALTER PROPERTY CustomerContact.salutation MANDATORY true
+  ALTER PROPERTY CustomerContact.firstname MANDATORY true
+  ALTER PROPERTY CustomerContact.surename MANDATORY true
+  ALTER PROPERTY CustomerContact.phone MANDATORY true
+  ALTER PROPERTY CustomerContact.mobilePhone MANDATORY true
+  ALTER PROPERTY CustomerContact.emailAddress MANDATORY true
 
-CREATE INDEX Customer.customerId UNIQUE
-CREATE INDEX CustomerContact.emailAddress UNIQUE
+  CREATE INDEX CustomerContact.emailAddress UNIQUE
 
-ALTER PROPERTY Customer.customerId MANDATORY true
-ALTER PROPERTY Customer.companyName MANDATORY true
-ALTER PROPERTY Customer.street MANDATORY true
-ALTER PROPERTY Customer.street2 MANDATORY true
-ALTER PROPERTY Customer.postcode MANDATORY true
-ALTER PROPERTY Customer.country MANDATORY true
-ALTER PROPERTY Customer.city MANDATORY true
-ALTER PROPERTY Customer.contacts MANDATORY true
-ALTER PROPERTY Customer.users MANDATORY true
-ALTER PROPERTY Customer.parentCustomer MANDATORY true
+  console.log "Creating process for CustomerContact class is done."
+}
 
-ALTER PROPERTY CustomerContact.type MANDATORY true
-ALTER PROPERTY CustomerContact.salutation MANDATORY true
-ALTER PROPERTY CustomerContact.firstname MANDATORY true
-ALTER PROPERTY CustomerContact.surename MANDATORY true
-ALTER PROPERTY CustomerContact.phone MANDATORY true
-ALTER PROPERTY CustomerContact.mobilePhone MANDATORY true
-ALTER PROPERTY CustomerContact.emailAddress MANDATORY true
+let customerClass = SELECT FROM (SELECT expand(classes) FROM metadata:schema) WHERE name = 'Customer';
 
+if ($customerClass.size() == 0) {
+  console.log "Customer class not exists! Start creating process."
+
+  CREATE Class Customer
+  CREATE PROPERTY Customer.customerId DOUBLE
+  CREATE PROPERTY Customer.companyName STRING
+  CREATE PROPERTY Customer.street STRING
+  CREATE PROPERTY Customer.street2 STRING
+  CREATE PROPERTY Customer.postcode DOUBLE
+  CREATE PROPERTY Customer.country STRING
+  CREATE PROPERTY Customer.city STRING
+  CREATE PROPERTY Customer.vatid DOUBLE
+
+  CREATE PROPERTY Customer.contacts LINKSET CustomerContact
+  CREATE PROPERTY Customer.users LINKSET OUser
+  CREATE PROPERTY Customer.parentCustomer LINK Customer
+
+  ALTER PROPERTY Customer.customerId MANDATORY true
+  ALTER PROPERTY Customer.companyName MANDATORY true
+  ALTER PROPERTY Customer.street MANDATORY true
+  ALTER PROPERTY Customer.street2 MANDATORY true
+  ALTER PROPERTY Customer.postcode MANDATORY true
+  ALTER PROPERTY Customer.country MANDATORY true
+  ALTER PROPERTY Customer.city MANDATORY true
+  ALTER PROPERTY Customer.contacts MANDATORY true
+  ALTER PROPERTY Customer.users MANDATORY true
+  ALTER PROPERTY Customer.parentCustomer MANDATORY true
+
+  CREATE INDEX Customer.customerId UNIQUE
+
+  console.log "Creating process for Customer class is done."
+}
+
+let customerIdSeqOSequence = SELECT FROM OSequence WHERE name = 'customerIdSeq';
+
+if ($customerIdSeqOSequence.size() == 0) {
+  console.log "SEQUENCE customerIdSeq not exists! Start creating process."
+
+  CREATE SEQUENCE customerIdSeq TYPE ORDERED
+
+  console.log "Creating process for SEQUENCE customerIdSeq is done."
+}
+
+return true
