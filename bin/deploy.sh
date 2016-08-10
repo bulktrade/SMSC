3a45d5411204046e0e4a43798b2085a0198702ef
Sergej Kunz <info@bulktrade.de>
2016-08-10 14:42:52 +0200

    fix deployment

---
+++ bin/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mvn clean install
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
+	mvn heroku:deploy -Dmaven.test.skip=true -Dmaven.integration-test.skip=true -Dmaven.javadoc.skip=true
+fi
