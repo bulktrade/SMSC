64df1251aa7911198ad932cf7968737da5b0ddb2
Dmitrij Kuba <dimakuba124@gmail.com>
2016-08-12 09:10:47 +0300

    changed the generation a breadcrumb use parent propery

--- modules/admin/src/app/breadcrumb/breadcrumb.service.ts
+++ modules/admin/src/app/breadcrumb/breadcrumb.service.ts
@@ -8,42 +8,42 @@ export class BreadcrumbService {
 
     constructor(public router:Router,
                 public route:ActivatedRoute) {
-        this.init();
+        this.childs = new Array();
+        this.name = this.route.component['name'];
+        this.chainBreadcrumbItems(this.route);
     }
 
-    init() {
-        let paths = [];
-        let path = '';
-        this.childs = new Array();
-
-        paths = this.router.url.split('/');
-
-        this.router.url.split('/').forEach(() => {
-            paths.forEach((ph, item) => {
-                if (item) {
-                    path += '/' + ph;
-                }
-            });
-
-            let nameChild = paths[paths.length - 1];
-
-            if (nameChild === '') {
-                nameChild = this.route.component['name'];
-            }
-
-            this.childs.push({
-                'name': nameChild,
-                'router': path,
-            });
-            path = '';
-            paths = paths.splice(0, paths.length - 1);
-        });
-
-        if (this.childs.length) {
-            this.childs = this.childs.splice(0, this.childs.length - 1).reverse();
-            this.name = this.childs[this.childs.length - 1].name;
+    chainBreadcrumbItems(route:ActivatedRoute) {
+        if (route.component['name'] === 'Navigation') {
+            return;
         }
 
+        this.childs.push({
+            'name': route.component['name'],
+            'router': this.chainPaths(route.pathFromRoot),
+        });
+
+        this.chainBreadcrumbItems(route.parent);
+    }
+
+    chainPaths(paths): string {
+        let result:string = '';
+
+        for (let item = 2; item < paths.length; item++) {
+            if (paths[item].routeConfig.path !== '') {
+                if (item !== paths.length - 1) {
+                    result += paths[item].routeConfig.path + '/';
+                } else {
+                    result += paths[item].routeConfig.path;
+                }
+            }
+        }
+
+        if (result === '') {
+            return '/';
+        }
+
+        return result;
     }
 
     navigateTo(router) {
