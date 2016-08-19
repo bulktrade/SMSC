366425a3a52830bf0ed4a50bac856f4fd580326e
Dmitrij Kuba <dimakuba124@gmail.com>
2016-08-19 12:39:31 +0300

    fix

--- modules/admin/src/app/crud/crudCreate/crud.create.spec.ts
+++ modules/admin/src/app/crud/crudCreate/crud.create.spec.ts
@@ -2,14 +2,18 @@ import {
     inject,
     addProviders
 } from '@angular/core/testing';
-import {CrudCreate} from "./crud.create.component";
-import {CRUD_PROVIDERS} from "../common/crudProviders";
+import { CrudCreate } from "./crud.create.component";
+import { CRUD_PROVIDERS } from "../common/crudProviders";
+import { Location } from "@angular/common";
+
+class MockLocation {};
 
 describe('Crud Create', () => {
     beforeEach(() => {
         addProviders([
             ...CRUD_PROVIDERS,
-            CrudCreate
+            CrudCreate,
+            { provide: Location, useClass: MockLocation }
         ]);
     });
 
