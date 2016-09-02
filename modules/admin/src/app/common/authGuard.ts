04e25b712706945be48485fc6336a019abf5536a
Kuba Dmitrij <dimakuba124@gmail.com>
2016-09-02 13:29:02 +0300

    rollback

--- modules/admin/src/app/common/authGuard.ts
+++ modules/admin/src/app/common/authGuard.ts
@@ -5,15 +5,15 @@ import { TokenService } from "../services/auth/token.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(protected router:Router, protected tokenService:TokenService) {
+    constructor(protected router: Router, protected tokenService: TokenService) {
     }
 
-    canActivate(route:ActivatedRouteSnapshot, state:RouterStateSnapshot):Observable<boolean> | boolean {
-        // if (!this.tokenService.isTokenExpired()) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+        if (!this.tokenService.isTokenExpired()) {
             return true;
-        // }
-        //
-        // this.router.navigateByUrl('/login');
-        // return false;
+        }
+        
+        this.router.navigateByUrl('/login');
+        return false;
     }
 }
