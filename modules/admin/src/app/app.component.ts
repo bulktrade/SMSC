d1300920e6ab81c19e7f4a72226e843128886dba
Kuba Dmitrij <dimakuba124@gmail.com>
2016-09-04 21:10:28 +0300

    ng2-select replaced with md-select

--- modules/admin/src/app/app.component.ts
+++ modules/admin/src/app/app.component.ts
@@ -12,7 +12,6 @@ import { TranslateService } from "ng2-translate";
         require('ag-grid/dist/styles/ag-grid.css'),
         require('ag-grid/dist/styles/theme-material.css'),
         require('bootstrap-material-design/dist/css/bootstrap-material-design.css'),
-        // require('ng2-select/components/css/ng2-select.css') @todo not working with ng2 RC6
     ],
     styles: [
         require('./app.scss')
