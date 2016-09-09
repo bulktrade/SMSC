19752e91b032ddf991d2120aba41139d83ccdf9a
Sergej Kunz <info@bulktrade.de>
2016-09-09 22:46:28 +0200

    webpack prod public path fix

--- modules/admin/config/webpack.prod.js
+++ modules/admin/config/webpack.prod.js
@@ -85,7 +85,12 @@ module.exports = function(env) {
              *
              * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
              */
-            chunkFilename: '[id].[chunkhash].chunk.js'
+            chunkFilename: '[id].[chunkhash].chunk.js',
+
+            /**
+             * Public path.
+             */
+            publicPath: "/resources/admin/"
 
         },
 
