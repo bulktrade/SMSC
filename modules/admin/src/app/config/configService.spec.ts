90bf195cd1000333e56dbe0f1b275e33a4b9ba25
Kuba Dmitrij <dimakuba124@gmail.com>
2016-09-11 10:44:08 +0300

    created mock for XMLHttpRequest + test for ConfigService

--- modules/admin/src/app/config/configService.spec.ts
+++ modules/admin/src/app/config/configService.spec.ts
@@ -1,36 +1,68 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { ConfigService } from "./configService";
-import { ConfigServiceMock } from "../common/mock/configServiceMock";
+import { HttpModule } from "@angular/http";
+import { XMLHttpRequestMock } from "../common/mock/XMLHttpRequestMock";
+import { Observable } from "rxjs";
 import { Config } from "./config";
 
-
-// @todo test is not right. You are not testing ConfigService. You are testing ConfigServiceMock.
-// Mock XHR instance. Here is an example: http://jsfiddle.net/eitanp461/tkSRC/
 describe('Config Service', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             providers: [
-                { provide: ConfigService, useClass: ConfigServiceMock },
+                ConfigService,
             ],
+            imports: [
+                HttpModule
+            ]
         });
     });
 
-    it('should get the parameters config.json', inject([ConfigService], (configService: ConfigService) => {
-        let config: Config = {
-            orientDBUrl: "/orientdb",
-            orientDBDatabase: "smsc",
-            i18nPath: "assets/i18n",
-            debug: false
+    it('should get config.json settings', inject([ConfigService], (configService) => {
+        let responseText = {
+            "orientDBUrl": "/orientdb",
+            "orientDBDatabase": "smsc",
+            "i18nPath": "assets/i18n",
+            "debug": false
         };
 
-        configService.configStream.subscribe(res => {
-            expect(res.orientDBUrl).toEqual(config.orientDBUrl);
-            expect(res.orientDBDatabase).toEqual(config.orientDBDatabase);
-            expect(res.i18nPath).toEqual(config.i18nPath);
-            expect(res.debug).toEqual(config.debug);
-        })
+        let mockXHR = new XMLHttpRequestMock();
+        mockXHR.responseText = JSON.stringify(responseText);
+        spyOn(window, "XMLHttpRequest").and.returnValue(mockXHR);
+
+        expect(configService.configStream instanceof Observable).toBeTruthy();
+
+        configService.configStream.subscribe((res: Config) => {
+            expect(res.orientDBUrl).toEqual(responseText.orientDBUrl);
+            expect(res.orientDBDatabase).toEqual(responseText.orientDBDatabase);
+            expect(res.i18nPath).toEqual(responseText.i18nPath);
+            expect(res.debug).toEqual(responseText.debug);
+        });
+    }));
+
+    it('should get an error on non-existent path', inject([ConfigService], (configService) => {
+        let responseText = {
+            "orientDBUrl": "/orientdb",
+            "orientDBDatabase": "smsc",
+            "i18nPath": "assets/i18n",
+            "debug": false
+        };
+
+        let notFound: string = 'Not found!';
+
+        let mockXHR = new XMLHttpRequestMock();
+        mockXHR.responseText = JSON.stringify(responseText);
+        mockXHR.status = 404;
+        spyOn(window, "XMLHttpRequest").and.returnValue(mockXHR);
+
+        expect(configService.configStream instanceof Observable).toBeTruthy();
+
+        configService.configStream.subscribe((res: Config) => {
+        }, err => {
+            expect(err.status).toEqual(404);
+        });
     }));
 
 });
+
 
