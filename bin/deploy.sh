dd04558d0e63b5b8cf9bf49f0ee75be0d171fe9d
Sergej Kunz <s.kunz@bulktrade.org>
2016-09-23 15:39:38 +0200

    fixes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,6 +2,11 @@
 set -x
 set -e
 
+export ADMIN_ORIENTDB_URL=http://localhost:24080
+export ADMIN_ORIENTDB_DATABASE=smsc
+export EMBEDDED_ORIENTDB_ENABLED=1
+export ORIENTDB_HTTP_PORT_RANGE=24080-24080
+
 mvn clean install
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
