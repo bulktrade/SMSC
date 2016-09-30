7567672a30ffb59357abae331c14e17dd092fa96
stas0 <stasbamiza3@gmail.com>
2016-09-30 11:05:49 +0300

    Edit

--- modules/admin/config/protractor.common.js
+++ modules/admin/config/protractor.common.js
@@ -8,7 +8,7 @@ exports.config = {
     specs: [
         //helpers.root('src/**/**.e2e.ts'),
         //helpers.root('src/**/*.e2e.ts')
-        helpers.root('src/**/dashboard.e2e.ts')
+        //helpers.root('src/**/dashboard.e2e.ts')
     ],
     exclude: [],
 
