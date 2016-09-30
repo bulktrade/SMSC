55109e3fdaa0bb0cce65fe8cf7acaf00bc384c19
stas0 <stasbamiza3@gmail.com>
2016-09-30 11:56:13 +0300

    Edit

--- modules/admin/config/spec-bundle.js
+++ modules/admin/config/spec-bundle.js
@@ -48,7 +48,8 @@ testing.TestBed.initTestEnvironment(
  * we say do this recursively
  */
 var firstTestContext = require.context('../src/app/config', false, /configService\.spec\.ts/);
-var testContext = require.context('../src', true, /dashboard_box_create\.spec\.ts/);
+//var testContext = require.context('../src', true, /dashboard_box_create\.spec\.ts/);
+var testContext = require.context('../src', true, /\.spec\.ts/);
 
 /*
  * get all the files, for each file, call the context function
