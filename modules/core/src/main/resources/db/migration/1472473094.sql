8b05aab96e4c27f862e1017562855bd81b84d4ee
stas0 <stasbamiza3@gmail.com>
2016-09-30 20:16:58 +0300

    Edit sql

--- modules/core/src/main/resources/db/migration/1472473094.sql
+++ modules/core/src/main/resources/db/migration/1472473094.sql
@@ -1,7 +1,7 @@
-let OUserRID = SELECT @rid FROM OUser WHERE name = 'admin';
+let OUserRID = SELECT * FROM OUser WHERE name = 'admin';
 let Dashboard = SELECT FROM (SELECT expand(classes) FROM metadata:schema) WHERE name = 'Dashboard';
 
-if ($DashboardBox.size() == 0) {
+if ($Dashboard.size() == 0) {
   console.log "Dashboard class not exists! Start creating process."
 
   CREATE Class Dashboard
@@ -15,13 +15,13 @@ if ($DashboardBox.size() == 0) {
 
   CREATE INDEX UNIQUE_NAME_USER ON Dashboard (name, user) UNIQUE
 
-  let defaultDashboard = INSERT INTO Dashboard SET name = 'default', icon = 'user', user = $OUserRID
+  let defaultDashboard = INSERT INTO Dashboard SET name = 'default', icon = 'user', user = $OUserRID['@rid'][0]
 
   console.log "Creating process for Dashboard class is done."
 
   let DashboardBoxType = SELECT FROM (SELECT expand(classes) FROM metadata:schema) WHERE name = 'DashboardBoxType';
 
-  if (DashboardBoxType.size() == 0) {
+  if ($DashboardBoxType.size() == 0) {
     console.log "DashboardBoxType class not exists! Start creating process."
 
     CREATE Class DashboardBoxType
@@ -47,10 +47,14 @@ if ($DashboardBox.size() == 0) {
   if ($DashboardBox.size() == 0) {
     console.log "DashboardBox class not exists! Start creating process."
 
+    let statusType = select * from DashboardBoxType where name = 'status'
+    let chartType = select * from DashboardBoxType where name = 'chart'
+    let defaultDashboard = select * from Dashboard where name = 'default'
+
     CREATE Class DashboardBox
 
-    CREATE PROPERTY DashboardBox.width INTEGER
-    CREATE PROPERTY DashboardBox.height INTEGER
+    CREATE PROPERTY DashboardBox.width EMBEDDEDLIST
+    CREATE PROPERTY DashboardBox.height EMBEDDEDLIST
     CREATE PROPERTY DashboardBox.order INTEGER
     CREATE PROPERTY DashboardBox.name STRING
     CREATE PROPERTY DashboardBox.description STRING
@@ -58,7 +62,9 @@ if ($DashboardBox.size() == 0) {
     CREATE PROPERTY DashboardBox.type LINK DashboardBoxType
 
     ALTER PROPERTY DashboardBox.width MANDATORY true
+    ALTER PROPERTY DashboardBox.width CUSTOM type='25,50,75,100'
     ALTER PROPERTY DashboardBox.height MANDATORY true
+    ALTER PROPERTY DashboardBox.height CUSTOM type='25,50,75,100'
     ALTER PROPERTY DashboardBox.order MANDATORY true
     ALTER PROPERTY DashboardBox.name MANDATORY true
     ALTER PROPERTY DashboardBox.description MANDATORY true
@@ -67,16 +73,38 @@ if ($DashboardBox.size() == 0) {
 
     CREATE INDEX UNIQUE_DASHBOARD_ORDER ON DashboardBox (dashboard, order) UNIQUE
 
-    INSERT INTO DashboardBox SET width = 25, height = 25, order = 1, dashboard = $defaultDashboard['@rid'], type = $statusType['@rid'], name = 'Box 1', description = 'Box 1 desc'
-    INSERT INTO DashboardBox SET width = 25, height = 25, order = 2, dashboard = $defaultDashboard['@rid'], type = $statusType['@rid'], name = 'Box 2', description = 'Box 2 desc'
-    INSERT INTO DashboardBox SET width = 25, height = 25, order = 3, dashboard = $defaultDashboard['@rid'], type = $statusType['@rid'], name = 'Box 3', description = 'Box 3 desc'
-    INSERT INTO DashboardBox SET width = 25, height = 25, order = 4, dashboard = $defaultDashboard['@rid'], type = $statusType['@rid'], name = 'Box 4', description = 'Box 4 desc'
+    INSERT INTO DashboardBox SET width = 25, height = 25, order = 1, dashboard = $defaultDashboard['@rid'][0], type = $statusType['@rid'][0], name = 'Box 1', description = 'Box 1 desc'
+    INSERT INTO DashboardBox SET width = 25, height = 25, order = 2, dashboard = $defaultDashboard['@rid'][0], type = $statusType['@rid'][0], name = 'Box 2', description = 'Box 2 desc'
+    INSERT INTO DashboardBox SET width = 25, height = 25, order = 3, dashboard = $defaultDashboard['@rid'][0], type = $statusType['@rid'][0], name = 'Box 3', description = 'Box 3 desc'
+    INSERT INTO DashboardBox SET width = 25, height = 25, order = 4, dashboard = $defaultDashboard['@rid'][0], type = $statusType['@rid'][0], name = 'Box 4', description = 'Box 4 desc'
 
-    INSERT INTO DashboardBox SET width = 50, height = 25, order = 5, dashboard = $defaultDashboard['@rid'], type = $chartType['@rid'], name = 'Box 5', description = 'Box 5 desc'
-    INSERT INTO DashboardBox SET width = 50, height = 25, order = 6, dashboard = $defaultDashboard['@rid'], type = $chartType['@rid'], name = 'Box 6', description = 'Box 6 desc'
+    INSERT INTO DashboardBox SET width = 50, height = 25, order = 5, dashboard = $defaultDashboard['@rid'][0], type = $chartType['@rid'][0], name = 'Box 5', description = 'Box 5 desc'
+    INSERT INTO DashboardBox SET width = 50, height = 25, order = 6, dashboard = $defaultDashboard['@rid'][0], type = $chartType['@rid'][0], name = 'Box 6', description = 'Box 6 desc'
 
     console.log "Creating process for DashboardBox class is done."
   }
+
+  console.log "Insert into Crud Meta"
+
+  INSERT INTO CrudClassMetaData SET class = 'Dashboard', titleColumns = 'name', editable = true
+  INSERT INTO CrudClassMetaData SET class = 'DashboardBox', titleColumns = 'name', editable = true
+  INSERT INTO CrudClassMetaData SET class = 'DashboardBoxType', titleColumns = 'name', editable = true
+
+  let CrudClassMetaData_Dashboard = SELECT * FROM CrudClassMetaData WHERE class = 'Dashboard'
+  let CrudClassMetaData_DashboardBox = SELECT * FROM CrudClassMetaData WHERE class = 'DashboardBox'
+  let CrudClassMetaData_DashboardBoxType = SELECT * FROM CrudClassMetaData WHERE class = 'DashboardBoxType'
+
+  INSERT INTO CrudMetaFormData  SET property = 'name', editable = true, visible = true, order = 1, crudClassMetaData = $CrudClassMetaData_Dashboard['@rid'][0]
+
+  INSERT INTO CrudMetaFormData  SET property = 'name', editable = true, visible = true, order = 1, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'description', editable = true, visible = true, order = 2, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'type', editable = true, visible = true, order = 3, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'dashboard', editable = true, visible = true, order = 4, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'width', editable = true, visible = true, order = 5, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'height', editable = true, visible = true, order = 6, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+  INSERT INTO CrudMetaFormData  SET property = 'order', editable = true, visible = true, order = 7, crudClassMetaData = $CrudClassMetaData_DashboardBox['@rid'][0]
+
+  INSERT INTO CrudMetaFormData  SET property = 'name', editable = true, visible = true, order = 1, crudClassMetaData = $CrudClassMetaData_DashboardBoxType['@rid'][0]
 }
 
 return true
