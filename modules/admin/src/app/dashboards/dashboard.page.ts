c53fed24e48eca37d203359430fe4683aa17d714
Sergej Kunz <info@bulktrade.de>
2016-10-04 22:10:28 +0200

    cleanup + fixes

--- modules/admin/src/app/dashboards/dashboard.page.ts
+++ modules/admin/src/app/dashboards/dashboard.page.ts
@@ -1,6 +1,5 @@
 import { browser } from "protractor/built/index";
-import { LoginPage } from "../pages/login.page";
-import {WaitUntil} from "../pages/common/waitUntilReady";
+import { LoginPage } from "../login/login.page";
 
 export class Dashboard {
     public dashboard = element(by.css('.dashboard'));
@@ -189,22 +188,22 @@ export class Dashboard {
         });
 
         /*return new Promise((resolve) => {
-            this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
-                this_.clickBySelector(selector, 1000).then(() => {
-                    selector = '.ag-body-container > div:first-child .ag-selection-checkbox img:nth-child(2)';
-                    this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
-                        this_.clickBySelector(selector).then(() => {
-                            /!*selector = '#addLink';
-                             this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
-                             this_.clickBySelector(selector).then(() => {
-                             resolve(true);
-                             });
-                             });*!/
-                        });
-                    });
-                });
-            });
-        });*/
+         this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
+         this_.clickBySelector(selector, 1000).then(() => {
+         selector = '.ag-body-container > div:first-child .ag-selection-checkbox img:nth-child(2)';
+         this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
+         this_.clickBySelector(selector).then(() => {
+         /!*selector = '#addLink';
+         this_.prot.wait(protractor.until.elementLocated(by.css(selector)), 5000).then((el: webdriver.IWebElement) => {
+         this_.clickBySelector(selector).then(() => {
+         resolve(true);
+         });
+         });*!/
+         });
+         });
+         });
+         });
+         });*/
     }
 
     /**
