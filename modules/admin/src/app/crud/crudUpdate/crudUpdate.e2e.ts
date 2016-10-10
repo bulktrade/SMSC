913833e1d5ce2e997ce4355bb9aad6a9d6b3700f
Kuba Dmitrij <dimakuba124@gmail.com>
2016-10-10 13:47:11 +0300

    created integration test for crudUpdate

---
+++ modules/admin/src/app/crud/crudUpdate/crudUpdate.e2e.ts
@@ -0,0 +1,58 @@
+import { UpdatePage } from './crudUpdate.page';
+
+describe('CRUD Update', () => {
+    let updatePage: UpdatePage;
+    let ptor = protractor.wrapDriver(browser.driver);
+
+    beforeEach(() => {
+        updatePage = new UpdatePage();
+        ptor = protractor.wrapDriver(browser.driver);
+    });
+
+    it('log in smsc.io', () => {
+        let width = 1024,
+            height = 768;
+        ptor.manage().window().setSize(width, height);
+
+        updatePage.crudPage.get();
+        updatePage.crudPage.login.login();
+        expect(updatePage.crudPage.isPresentLogo()).toBeTruthy();
+    });
+
+    it('should navigate to the customer', () => {
+        updatePage.crudPage.clickOnCustomers();
+        expect(updatePage.crudPage.isPresentCustomers()).toBeTruthy();
+    });
+
+    it('should navigate to edit', () => {
+        updatePage.clickOnEditIcon();
+        expect(updatePage.isPresentUpdateDirective()).toBeTruthy();
+    });
+
+    it('should be modified value', () => {
+        updatePage.sentKeysToEditableField();
+        updatePage.clickOnFormBtn();
+        updatePage.crudPage.clickOnBackBtn();
+        expect(updatePage.crudPage.isPresentCrudViewTag).toBeTruthy();
+    });
+
+    it('should navigate to edit', () => {
+        updatePage.clickOnEditIcon();
+        expect(updatePage.isPresentUpdateDirective()).toBeTruthy();
+    });
+
+    it('should be displayed the modified value', () => {
+        let expectedValue = 2;
+
+        updatePage.editableField.getAttribute('value')
+            .then((value) => {
+                expect(Number(value)).toEqual(expectedValue);
+            });
+    });
+
+    it('should logout', () => {
+        updatePage.crudPage.login.logout();
+        expect(updatePage.crudPage.login.isPresentLogin()).toBeTruthy();
+    });
+
+});
