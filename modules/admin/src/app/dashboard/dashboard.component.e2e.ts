7ab1cd1cc6e537a0af596416f7978c60ebbf5ca8
Dmitrij Kuba <dimakuba124@gmail.com>
2017-01-25 13:22:58 +0200

    disable e2e tests

--- modules/admin/src/app/dashboard/dashboard.component.e2e.ts
+++ modules/admin/src/app/dashboard/dashboard.component.e2e.ts
@@ -1,67 +1,67 @@
-import { Dashboard } from './dashboard.page';
-
-describe('DashboardComponent', () => {
-    let prot = protractor.wrapDriver(browser.driver);
-
-    prot.manage().window().setSize(1024, 1020);
-    browser.get(browser.baseUrl + '/');
-
-    beforeEach(() => {
-        this.dashboard = new Dashboard();
-        prot = protractor.wrapDriver(browser.driver);
-        this.dashboard.prot = prot;
-    });
-
-    //  Sign in
-    it('Try to login login', () => {
-        this.dashboard.login.login();
-    });
-
-    //  Check page title
-    it('Test title', () => {
-        let result = 'SMSC Admin';
-
-        this.dashboard.getTitle().then((res) => {
-            expect(this.dashboard.getTitle()).toBe(result);
-        });
-    });
-
-    //  Check by existing dashboard
-    it('Check by existing dashboard', () => {
-        this.dashboard.clickOnItemNavDashboard(prot).then(() => {
-            expect(this.dashboard.getDashboard()).toBeTruthy();
-        });
-    });
-
-    //  Switch to fullscreen mode
-    it('Click on fullscreen icon', () => {
-        this.dashboard.clickOnFullscreenIcon();
-    });
-
-    //  Open/Close crud box tool
-    it('Open/Close crud box tool', () => {
-        this.dashboard.toggleCloseIcon();
-    });
-
-    //  Change size mode
-    it('Switch height box mode', () => {
-        this.dashboard.clickOnSizeButtons();
-    });
-
-    //  Create box
-    it('Create box', () => {
-        this.dashboard.createBox();
-    });
-
-    it('Edit box', () => {
-        this.dashboard.editBox();
-    });
-
-    it('Remove box', () => {
-        this.dashboard.removeBox();
-    });
-
-    it('should logout', () => {
-        this.dashboard.login.logout();
-    });
-});
+// import { Dashboard } from './dashboard.page';
+//
+// describe('DashboardComponent', () => {
+//     let prot = protractor.wrapDriver(browser.driver);
+//
+//     prot.manage().window().setSize(1024, 1020);
+//     browser.get(browser.baseUrl + '/');
+//
+//     beforeEach(() => {
+//         this.dashboard = new Dashboard();
+//         prot = protractor.wrapDriver(browser.driver);
+//         this.dashboard.prot = prot;
+//     });
+//
+//     //  Sign in
+//     it('Try to login login', () => {
+//         this.dashboard.login.login();
+//     });
+//
+//     //  Check page title
+//     it('Test title', () => {
+//         let result = 'SMSC Admin';
+//
+//         this.dashboard.getTitle().then((res) => {
+//             expect(this.dashboard.getTitle()).toBe(result);
+//         });
+//     });
+//
+//     //  Check by existing dashboard
+//     it('Check by existing dashboard', () => {
+//         this.dashboard.clickOnItemNavDashboard(prot).then(() => {
+//             expect(this.dashboard.getDashboard()).toBeTruthy();
+//         });
+//     });
+//
+//     //  Switch to fullscreen mode
+//     it('Click on fullscreen icon', () => {
+//         this.dashboard.clickOnFullscreenIcon();
+//     });
+//
+//     //  Open/Close crud box tool
+//     it('Open/Close crud box tool', () => {
+//         this.dashboard.toggleCloseIcon();
+//     });
+//
+//     //  Change size mode
+//     it('Switch height box mode', () => {
+//         this.dashboard.clickOnSizeButtons();
+//     });
+//
+//     //  Create box
+//     it('Create box', () => {
+//         this.dashboard.createBox();
+//     });
+//
+//     it('Edit box', () => {
+//         this.dashboard.editBox();
+//     });
+//
+//     it('Remove box', () => {
+//         this.dashboard.removeBox();
+//     });
+//
+//     it('should logout', () => {
+//         this.dashboard.login.logout();
+//     });
+// });
--- modules/admin/src/app/navigation/navigation.component.e2e.ts
+++ modules/admin/src/app/navigation/navigation.component.e2e.ts
@@ -1,61 +1,61 @@
-import {NavigationPage} from "./navigation.page";
-
-describe('NavigationComponent', () => {
-    let navigator = new NavigationPage();
-    let ptor = protractor.wrapDriver(browser.driver);
-
-    beforeEach(() => {
-        ptor = protractor.wrapDriver(browser.driver);
-    });
-
-    it('should have a title', () => {
-        navigator.get();
-        let result = 'SMSC Admin';
-        expect(navigator.getTitle()).toBe(result);
-    });
-
-    it('login', () => {
-        navigator.login.login();
-        expect(navigator.isPresentLogo()).toBeTruthy();
-    });
-
-    it('should have dashboard', () => {
-        navigator.clickOnItemNavDashboard();
-        expect(navigator.isPresentDashboard()).toBeTruthy();
-    });
-
-    it('marked sub and main item navigation like active', () => {
-        navigator.hasClass(navigator.dashboardItem, 'active').then((data) => {
-            expect(data).toBeTruthy();
-        });
-    });
-
-    it('should have navigation directive', () => {
-        expect(navigator.isPresentNavDirective()).toBeTruthy();
-    });
-
-    it('should have customers', () => {
-        navigator.clickOnItemNavCustomers();
-        expect(navigator.isPresentCustomers()).toBeTruthy();
-    });
-
-    it('responsive navigation', () => {
-        let width = 900,
-            height = 1300;
-        ptor.manage().window().setSize(width, height);
-        navigator.sidebarDirective.getCssValue('width')
-            .then(value => {
-                expect(value).toEqual('auto');
-            });
-    });
-
-    // it('AngularJS Translations', () => {
-    //     let lang = navigator.getLanguage();
-    //
-    //     WaitUntil.waitUntil(navigator.dashboardTitle, ptor);
-    //     navigator.getDashboardText()
-    //         .then((text) => {
-    //             expect(lang).toEqual(text);
-    //         });
-    // });
-});
+// import {NavigationPage} from "./navigation.page";
+//
+// describe('NavigationComponent', () => {
+//     let navigator = new NavigationPage();
+//     let ptor = protractor.wrapDriver(browser.driver);
+//
+//     beforeEach(() => {
+//         ptor = protractor.wrapDriver(browser.driver);
+//     });
+//
+//     it('should have a title', () => {
+//         navigator.get();
+//         let result = 'SMSC Admin';
+//         expect(navigator.getTitle()).toBe(result);
+//     });
+//
+//     it('login', () => {
+//         navigator.login.login();
+//         expect(navigator.isPresentLogo()).toBeTruthy();
+//     });
+//
+//     it('should have dashboard', () => {
+//         navigator.clickOnItemNavDashboard();
+//         expect(navigator.isPresentDashboard()).toBeTruthy();
+//     });
+//
+//     it('marked sub and main item navigation like active', () => {
+//         navigator.hasClass(navigator.dashboardItem, 'active').then((data) => {
+//             expect(data).toBeTruthy();
+//         });
+//     });
+//
+//     it('should have navigation directive', () => {
+//         expect(navigator.isPresentNavDirective()).toBeTruthy();
+//     });
+//
+//     it('should have customers', () => {
+//         navigator.clickOnItemNavCustomers();
+//         expect(navigator.isPresentCustomers()).toBeTruthy();
+//     });
+//
+//     it('responsive navigation', () => {
+//         let width = 900,
+//             height = 1300;
+//         ptor.manage().window().setSize(width, height);
+//         navigator.sidebarDirective.getCssValue('width')
+//             .then(value => {
+//                 expect(value).toEqual('auto');
+//             });
+//     });
+//
+//     // it('AngularJS Translations', () => {
+//     //     let lang = navigator.getLanguage();
+//     //
+//     //     WaitUntil.waitUntil(navigator.dashboardTitle, ptor);
+//     //     navigator.getDashboardText()
+//     //         .then((text) => {
+//     //             expect(lang).toEqual(text);
+//     //         });
+//     // });
+// });
