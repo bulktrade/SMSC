61b73a0f84fafa9a558c973a036c0e1ec1b0c99e
Dmitrij Kuba <dimakuba124@gmail.com>
2017-02-01 12:39:49 +0200

    enable login and app e2e tests

--- modules/admin/src/app/app.component.e2e.ts
+++ modules/admin/src/app/app.component.e2e.ts
@@ -1,34 +1,34 @@
-// import { AppTest } from './app.page';
-// import { protractor, browser } from "protractor";
-//
-// describe('App', () => {
-//     let ptor = protractor.wrapDriver(browser.driver);
-//
-//     beforeEach(() => {
-//         this.apptest = new AppTest();
-//         ptor = protractor.wrapDriver(browser.driver);
-//     });
-//
-//     it('should have a title', () => {
-//         let width = 1024,
-//             height = 768;
-//         ptor.manage().window().setSize(width, height);
-//
-//         this.apptest.get();
-//         let subject = browser.getTitle();
-//         let result = 'SMSC Admin';
-//
-//         expect(subject).toEqual(result);
-//     });
-//
-//     it('should have input username', () => {
-//         let result = true;
-//         expect(this.apptest.isPresentUsername()).toEqual(result);
-//     });
-//
-//     it('should have input password', () => {
-//         let result = true;
-//         expect(this.apptest.isPresentPassword()).toEqual(result);
-//     });
-//
-// });
+import { AppTest } from './app.page';
+import { protractor, browser } from "protractor";
+
+describe('App', () => {
+    let ptor = protractor.wrapDriver(browser.driver);
+
+    beforeEach(() => {
+        this.apptest = new AppTest();
+        ptor = protractor.wrapDriver(browser.driver);
+    });
+
+    it('should have a title', () => {
+        let width = 1024,
+            height = 768;
+        ptor.manage().window().setSize(width, height);
+
+        this.apptest.get();
+        let subject = browser.getTitle();
+        let result = 'SMSC Admin';
+
+        expect(subject).toEqual(result);
+    });
+
+    it('should have input username', () => {
+        let result = true;
+        expect(this.apptest.isPresentUsername()).toEqual(result);
+    });
+
+    it('should have input password', () => {
+        let result = true;
+        expect(this.apptest.isPresentPassword()).toEqual(result);
+    });
+
+});
--- modules/admin/src/app/login/login.component.e2e.ts
+++ modules/admin/src/app/login/login.component.e2e.ts
@@ -1,29 +1,29 @@
-// import { LoginPage } from './login.page';
-// import { LoginModel } from './login.model';
-// import { browser, protractor } from 'protractor';
-//
-// describe('LoginComponent page', () => {
-//     let ptor = protractor.wrapDriver(browser.driver);
-//     let loginPage = new LoginPage();
-//
-//     beforeEach(() => {
-//         ptor = protractor.wrapDriver(browser.driver);
-//     });
-//
-//     it('should display login window', () => {
-//         loginPage.get();
-//         expect(loginPage.isPresentLoginWindow()).toBeTruthy();
-//     });
-//
-//     it('should display alert danger', () => {
-//         let incorrectData: LoginModel = new LoginModel('root', '123t', false);
-//         loginPage.fillLoginForm(incorrectData);
-//         expect(loginPage.isPresentErrorAlert()).toBeTruthy();
-//     });
-//
-//     it('is exist page 404 not found', () => {
-//         loginPage.getNotFound();
-//         expect(loginPage.isPresentNotFound).toBeTruthy();
-//     });
-//
-// });
+import { LoginPage } from './login.page';
+import { LoginModel } from './login.model';
+import { browser, protractor } from 'protractor';
+
+describe('LoginComponent page', () => {
+    let ptor = protractor.wrapDriver(browser.driver);
+    let loginPage = new LoginPage();
+
+    beforeEach(() => {
+        ptor = protractor.wrapDriver(browser.driver);
+    });
+
+    it('should display login window', () => {
+        loginPage.get();
+        expect(loginPage.isPresentLoginWindow()).toBeTruthy();
+    });
+
+    it('should display alert danger', () => {
+        let incorrectData: LoginModel = new LoginModel('root', '123t', false);
+        loginPage.fillLoginForm(incorrectData);
+        expect(loginPage.isPresentErrorAlert()).toBeTruthy();
+    });
+
+    it('is exist page 404 not found', () => {
+        loginPage.getNotFound();
+        expect(loginPage.isPresentNotFound).toBeTruthy();
+    });
+
+});
