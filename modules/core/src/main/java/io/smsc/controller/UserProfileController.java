e73a80de38818a1c4b99b80a1c4809539a865982
deacix <info@deacix.de>
2017-02-05 21:09:00 +0100

    Links added to /me call

--- modules/core/src/main/java/io/smsc/controller/UserProfileController.java
+++ modules/core/src/main/java/io/smsc/controller/UserProfileController.java
@@ -5,18 +5,23 @@ import io.smsc.repository.UserRepository;
 import io.smsc.security.model.JWTUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.hateoas.Resource;
+import org.springframework.data.rest.webmvc.support.RepositoryEntityLinks;
+import org.springframework.hateoas.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static org.springframework.hateoas.TemplateVariable.VariableType.REQUEST_PARAM;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RepositoryRestController
 public class UserProfileController {
     @Autowired
     UserRepository repository;
+
+    @Autowired
+    RepositoryEntityLinks entityLinks;
 
     @Autowired
     public UserProfileController(UserRepository repo) {
@@ -29,6 +34,16 @@ public class UserProfileController {
     @ResponseBody
     Resource<User> getLoggedUser() {
         JWTUser jwtUser = (JWTUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return new Resource<>(repository.findOne(jwtUser.getId()));
+        Resource<User> result = new Resource<>(repository.findOne(jwtUser.getId()));
+
+        Link userLink = entityLinks.linkToSingleResource(UserRepository.class, jwtUser.getId());
+        TemplateVariables templateVariables = new TemplateVariables(new TemplateVariable("projection", REQUEST_PARAM));
+
+        result.add(userLink.withSelfRel());
+        result.add(new Link((new UriTemplate(userLink.getHref(), templateVariables).toString()), "user"));
+        result.add(new Link((new UriTemplate(userLink.getHref().concat("/roles"))).toString(), "roles")); // @todo update after roles entitiy change to authorities.
+        result.add(new Link((new UriTemplate(userLink.getHref().concat("/dashboards"))).toString(), "dashboards"));
+
+        return result;
     }
 }
