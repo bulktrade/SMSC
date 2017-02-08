1f03c61f1a692f8553902016d0aa7d50055d68ea
Sergej Kunz <info@deacix.de>
2017-02-08 16:26:50 +0100

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,13 +2,8 @@
 set -x
 set -e
 
-export ADMIN_ORIENTDB_URL=http://localhost:24080
-export ADMIN_ORIENTDB_DATABASE=smsc
-export EMBEDDED_ORIENTDB_ENABLED=1
-export ORIENTDB_HTTP_PORT_RANGE=24080-24080
-
-mvn clean install
-
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-	mvn heroku:deploy -Dmaven.test.skip=true -Dmaven.integration-test.skip=true -Dmaven.javadoc.skip=true
+	mvn clean heroku:deploy -Dmaven.test.skip=true -Dmaven.integration-test.skip=true -Dmaven.javadoc.skip=true
+else
+	mvn clean install
 fi
