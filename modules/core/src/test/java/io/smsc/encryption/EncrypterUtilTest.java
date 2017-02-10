f350984b7ada84d628c7bf74fe22a69a9f53cc1d
Nazar Lipkovskyy <nazar.lipkovskyy@gmail.com>
2017-02-10 17:31:22 +0200

    Add encryption tests for existing user and customerUser, fix hibernate4 maven plugin to not export table droppings, remove hibernate4 maven plugin from general pom file, add connection properties to hibernate4 maven plugin for all databases

--- modules/core/src/test/java/io/smsc/encryption/EncrypterUtilTest.java
+++ modules/core/src/test/java/io/smsc/encryption/EncrypterUtilTest.java
@@ -1,15 +1,17 @@
 package io.smsc.encryption;
 
+import io.smsc.AbstractTest;
 import io.smsc.model.CustomerUser;
 import io.smsc.model.User;
 import io.smsc.util.EncrypterUtil;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 
 import static io.smsc.util.EncrypterUtil.removeCryptographyRestrictions;
 
-public class EncrypterUtilTest {
+public class EncrypterUtilTest extends AbstractTest {
 
     @Before
     public void removeCryptographyRestriction() {
@@ -17,10 +19,9 @@ public class EncrypterUtilTest {
     }
 
     @Test
-    public void testEncryptDecryptUserPassword() {
+    public void testNewUserEncryptDecryptPassword() {
         User user = new User();
         user.setPassword("john123456");
-        System.out.println("Raw password: " + user.getPassword());
         EncrypterUtil.encrypt(user);
         System.out.println("Encrypted password: " + user.getPassword());
         System.out.println("Salt: " + user.getSalt());
@@ -30,7 +31,7 @@ public class EncrypterUtilTest {
     }
 
     @Test
-    public void testEncryptDecryptCustomerUserPassword() {
+    public void testNewCustomerUserEncryptDecryptPassword() {
         CustomerUser customerUser = new CustomerUser();
         customerUser.setPassword("john123456");
         System.out.println("Raw password: " + customerUser.getPassword());
@@ -40,5 +41,37 @@ public class EncrypterUtilTest {
         EncrypterUtil.decrypt(customerUser);
         System.out.println("Decrypted password: " + customerUser.getPassword());
         Assert.assertEquals("john123456", customerUser.getPassword());
+    }
+
+    @Test
+    public void testExistingUserEncryptDecryptPassword() {
+        User user = new User();
+        user.setPassword("john123456");
+        String salt = KeyGenerators.string().generateKey();
+        user.setSalt(salt);
+        System.out.println("Raw password: " + user.getPassword());
+        EncrypterUtil.encrypt(user);
+        System.out.println("Encrypted password: " + user.getPassword());
+        System.out.println("Salt: " + user.getSalt());
+        EncrypterUtil.decrypt(user);
+        System.out.println("Decrypted password: " + user.getPassword());
+        Assert.assertEquals("john123456", user.getPassword());
+        Assert.assertEquals(salt, user.getSalt());
+    }
+
+    @Test
+    public void testExistingCustomerUserEncryptDecryptPassword() {
+        CustomerUser customerUser = new CustomerUser();
+        customerUser.setPassword("john123456");
+        String salt = KeyGenerators.string().generateKey();
+        customerUser.setSalt(salt);
+        System.out.println("Raw password: " + customerUser.getPassword());
+        EncrypterUtil.encrypt(customerUser);
+        System.out.println("Encrypted password: " + customerUser.getPassword());
+        System.out.println("Salt: " + customerUser.getSalt());
+        EncrypterUtil.decrypt(customerUser);
+        System.out.println("Decrypted password: " + customerUser.getPassword());
+        Assert.assertEquals("john123456", customerUser.getPassword());
+        Assert.assertEquals(salt, customerUser.getSalt());
     }
 }
