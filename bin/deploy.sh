cb84b2b65aca929b81e2a6b063c4523ea092b068
deacix <info@deacix.de>
2017-02-11 15:57:20 +0100

    Fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,12 +3,12 @@ set -x
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
     # Heroku deploy
-	mvn heroku:deploy
+	mvn heroku:deploy -PskipBuildAndTests
 
     # Docker build & deploy
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    mvn docker:build
+    mvn docker:build -PskipBuildAndTests
 
     # Release to public repository
-#	mvn release:clean release:prepare release:perform --settings ~/settings.xml
+#	mvn release:clean release:prepare release:perform --settings ~/settings.xml -PskipBuildAndTests
 fi
--- bin/report.sh
+++ bin/report.sh
@@ -2,4 +2,4 @@
 set -x
 set -e
 
-mvn -DrepoToken=$COVERALLS_REPO_TOKEN cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar
+mvn -DrepoToken=$COVERALLS_REPO_TOKEN cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar -PskipBuildAndTests
