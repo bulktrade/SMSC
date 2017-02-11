a649fc16dad938d034910ef476cf1e26c78fad13
deacix <info@deacix.de>
2017-02-11 17:43:57 +0100

    fix

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-set -x
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-    # Heroku deploy
-	mvn heroku:deploy -PskipBuildAndTests
-
     # Docker build & deploy
     docker login -e="$DOCKER_EMAIL" -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    mvn docker:build -PskipBuildAndTests
+    mvn deploy -PskipBuildAndTests
 
     # Release to public repository
 #	mvn release:clean release:prepare release:perform --settings ~/settings.xml -PskipBuildAndTests
