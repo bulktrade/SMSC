f4c5bfcaf794d5f876a83ae39ba6515b4d004f83
deacix <info@deacix.de>
2017-02-11 22:01:03 +0100

    added screenshots for protractor

--- modules/admin/config/protractor.common.js
+++ modules/admin/config/protractor.common.js
@@ -1,6 +1,8 @@
 require('ts-node/register');
 var helpers = require('./helpers');
 
+var HtmlReporter = require('protractor-html-screenshot-reporter');
+
 exports.config = {
     baseUrl: 'http://localhost:' + (process.env.TOMCAT_HTTP_PORT ? process.env.TOMCAT_HTTP_PORT : '8080') + '/admin',
 
@@ -23,8 +25,24 @@ exports.config = {
         defaultTimeoutInterval: 400000
     },
 
-    onPrepare: function() {
+    onPrepare: function () {
         browser.ignoreSynchronization = true;
+
+        jasmine.getEnv().addReporter(new HtmlReporter({
+            baseDirectory: './integration-tests/screenshots',
+            takeScreenShotsOnlyForFailedSpecs: true,
+            pathBuilder: function pathBuilder(spec, descriptions, results, capabilities) {
+                // Return '<browser>/<specname>' as path for screenshots:
+                // Example: 'firefox/list-should work'.
+                return path.join(capabilities.caps_.browser, descriptions.join('-'));
+            },
+            metaDataBuilder: function metaDataBuilder(spec, descriptions, results, capabilities) {
+                // Return the description of the spec and if it has passed or not:
+                return {
+                    description: descriptions.join(' '), passed: results.passed()
+                };
+            }
+        }));
     },
 
     /**
@@ -35,9 +53,9 @@ exports.config = {
      */
     useAllAngular2AppRoots: true,
 
-    onComplete: function() {
-        var printSessionId = function(jobName){
-            browser.getSession().then(function(session) {
+    onComplete: function () {
+        var printSessionId = function (jobName) {
+            browser.getSession().then(function (session) {
                 console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
             });
         };
