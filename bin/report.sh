5b319178dc0bd17daa3d3b6aa356b45ad8adde30
deacix <info@deacix.de>
2017-02-11 14:22:39 +0100

    Fix

--- bin/report.sh
+++ bin/report.sh
@@ -2,4 +2,4 @@
 set -x
 set -e
 
-mvn -DrepoToken=$COVERALLS_REPO_TOKEN cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar versioneye:update -Dmaven.test.skip=true -Dmaven.integration-test.skip=true -Dmaven.javadoc.skip=true
+mvn -DrepoToken=$COVERALLS_REPO_TOKEN cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar
