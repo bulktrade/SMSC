0033b92abff1c8466d2616b95772d63f5c1cd09c
Sergej Kunz <info@deacix.de>
2017-02-14 11:24:53 +0100

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,6 +7,11 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; t
     # Docker push
     mvn -f modules/core docker:push -Dmaven.exec.skip=true
 
+    # Raise the version
+    mvn release:clean
+    mvn release:prepare
+    mvn release:perform
+
     # Release to public repository
     mvn --settings sonatype-settings.xml -PskipBuildAndTests -B deploy
 fi
