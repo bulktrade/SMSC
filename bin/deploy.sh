3a0dee2ffb29b2d830261e8ac350ae1083a8c22c
Sergej Kunz <info@deacix.de>
2017-02-14 11:15:06 +0100

    Update deploy.sh

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,16 +4,14 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; t
     # Heroku deploy
 	mvn heroku:deploy -PskipBuildAndTests
 
-    # Docker build & push
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    mvn -f modules/core docker:build -DpushImage -Dmaven.exec.skip=true
+    # Docker push
+    mvn -f modules/core docker:push -Dmaven.exec.skip=true
 
     # Release to public repository
     mvn --settings sonatype-settings.xml -PskipBuildAndTests -B deploy
 fi
 
 if [ "$TRAVIS_BRANCH" != "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-    # Docker build & push
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-    mvn -f modules/core docker:build -Dmaven.exec.skip=true -DpushImageTags -DdockerImageTags=${TRAVIS_BRANCH//\//-}
+    # Docker push for feature branches
+    docker push smscio/smsc:${TRAVIS_BRANCH//\//-}
 fi
