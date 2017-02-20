1632aa0d3f06355c1ab64c140603c99569f4a8c1
Dmitrij Kuba <dimakuba124@gmail.com>
2017-02-20 14:11:02 +0200

    create protractor-helpers

--- modules/admin/config/protractor.common.js
+++ modules/admin/config/protractor.common.js
@@ -2,46 +2,47 @@ require('ts-node/register');
 var helpers = require('./helpers');
 
 exports.config = {
-    baseUrl: 'http://localhost:' + (process.env.TOMCAT_HTTP_PORT ? process.env.TOMCAT_HTTP_PORT : '8080') + '/admin',
+  baseUrl: 'http://localhost:' + (process.env.TOMCAT_HTTP_PORT ? process.env.TOMCAT_HTTP_PORT : '8080') + '/admin',
 
-    // use `npm run e2e`
-    specs: [
-        helpers.root('src/**/**.e2e.ts'),
-        helpers.root('src/**/*.e2e.ts')
-    ],
-    exclude: [],
+  // use `npm run e2e`
+  specs: [
+    helpers.root('src/app/login/login.component.e2e.ts'),
+    helpers.root('src/**/**.e2e.ts'),
+    helpers.root('src/**/*.e2e.ts')
+  ],
+  exclude: [],
 
-    framework: 'jasmine2',
+  framework: 'jasmine2',
 
-    allScriptsTimeout: 110000,
+  allScriptsTimeout: 110000,
 
-    jasmineNodeOpts: {
-        showTiming: true,
-        showColors: true,
-        isVerbose: true,
-        includeStackTrace: true,
-        defaultTimeoutInterval: 400000
-    },
+  jasmineNodeOpts: {
+    showTiming: true,
+    showColors: true,
+    isVerbose: true,
+    includeStackTrace: true,
+    defaultTimeoutInterval: 400000
+  },
 
-    onPrepare: function () {
-        browser.ignoreSynchronization = true;
-    },
+  onPrepare: function () {
+    browser.ignoreSynchronization = true;
+  },
 
-    /**
-     * Angular 2 configuration
-     *
-     * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
-     * `rootEl`
-     */
-    useAllAngular2AppRoots: true,
+  /**
+   * Angular 2 configuration
+   *
+   * useAllAngular2AppRoots: tells Protractor to wait for any angular2 apps on the page instead of just the one matching
+   * `rootEl`
+   */
+  useAllAngular2AppRoots: true,
 
-    onComplete: function () {
-        var printSessionId = function (jobName) {
-            browser.getSession().then(function (session) {
-                console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
-            });
-        };
+  onComplete: function () {
+    var printSessionId = function (jobName) {
+      browser.getSession().then(function (session) {
+        console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+      });
+    };
 
-        printSessionId("Admin Module");
-    }
+    printSessionId("Admin Module");
+  }
 };
---
+++ modules/admin/src/app/shared/protractor-helpers.ts
@@ -0,0 +1,23 @@
+import {ElementFinder, browser} from "protractor";
+import {EC} from "./expected-conditions";
+import {promise as wdpromise} from "selenium-webdriver";
+
+export class ProtractorHelpers {
+    readonly timeout: number = 5000;
+
+    clickOnElement(element: ElementFinder) {
+        browser.wait(EC.elementToBeClickable(element), this.timeout);
+        element.click();
+    }
+
+    isElementPresence(element: ElementFinder): wdpromise.Promise<boolean> {
+        browser.wait(EC.presenceOf(element), this.timeout);
+        return element.isPresent();
+    }
+
+    sendKeys(element: ElementFinder, value: string) {
+        browser.wait(EC.elementToBeClickable(element), this.timeout);
+        element.clear();
+        element.sendKeys(value);
+    }
+}
