67a7554efcf0f3b076ff56fc4118623a83f7af14
deacix <info@deacix.de>
2017-02-25 20:27:01 +0100

    fix

--- bin/build.sh
+++ bin/build.sh
@@ -4,7 +4,7 @@ mvn clean install
 
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
     # Raise the version
-    mvn release:clean release:prepare release:perform --settings sonatype-settings.xml
+    mvn -B release:clean release:prepare release:perform --settings sonatype-settings.xml
 fi
 
 # Docker login
