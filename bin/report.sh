5a8f329f423fa5808bdd2c3d6c758759660c34cb
Sergej Kunz <info@deacix.de>
2017-02-27 11:29:47 +0100

    Update report.sh

--- bin/report.sh
+++ bin/report.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] ; then
-  mvn -B -DrepoToken=$COVERALLS_REPO_TOKEN clean test cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/site/cobertura/coverage.xml -DprojectToken=$CODACY_PROJECT_TOKEN -DapiToken=$CODACY_API_TOKEN -Dmaven.exec.skip=true -DskipITs
-fi
+mvn -B -DrepoToken=$COVERALLS_REPO_TOKEN clean test cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/site/cobertura/coverage.xml -DprojectToken=$CODACY_PROJECT_TOKEN -DapiToken=$CODACY_API_TOKEN -Dmaven.exec.skip=true -DskipITs
