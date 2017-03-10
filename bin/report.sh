7c9c23ebb9c027276bf31105f1a4ddda8677f661
deacix <info@deacix.de>
2017-03-10 12:47:12 +0100

    fixed reports

--- bin/report.sh
+++ bin/report.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-mvn -B -DrepoToken=$COVERALLS_REPO_TOKEN clean test cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/site/cobertura/coverage.xml -DprojectToken=$CODACY_PROJECT_TOKEN -DapiToken=$CODACY_API_TOKEN -Dmaven.exec.skip=true -DskipITs
+mvn -B -DrepoToken=$COVERALLS_REPO_TOKEN cobertura:cobertura coveralls:report org.jacoco:jacoco-maven-plugin:prepare-agent sonar:sonar -Dmaven.exec.skip=true -DskipITs
+mvn -B com.gavinmogan:codacy-maven-plugin:coverage -DcoverageReportFile=target/site/cobertura/coverage.xml -DprojectToken=$CODACY_PROJECT_TOKEN -DapiToken=$CODACY_API_TOKEN -PskipBuildAndTests
