87ae854f92c4eca91ee437c9c6f26a4d10749d81
Dmitrij Kuba <dimakuba124@gmail.com>
2017-09-06 16:33:23 +0300

    fixes

--- modules/admin/src/app/dashboard/dashboard-update/dashboard-update.resolve.ts
+++ modules/admin/src/app/dashboard/dashboard-update/dashboard-update.resolve.ts
@@ -2,6 +2,7 @@ import {ActivatedRouteSnapshot, RouterStateSnapshot, Resolve} from "@angular/rou
 import {Injectable} from "@angular/core";
 import {DashboardService} from "../dashboard.service";
 import {Dashboard} from "../dashboard.model";
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class DashboardUpdateResolve implements Resolve<Dashboard> {
@@ -9,7 +10,7 @@ export class DashboardUpdateResolve implements Resolve<Dashboard> {
     constructor(public dashboardService: DashboardService) {
     }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Dashboard> {
         return this.dashboardService.getResourceById(Number(route.params['id']));
     }
 }
